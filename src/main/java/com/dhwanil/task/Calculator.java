8d24acc08077c12e517b2d0d1df7c32861dc1c9e
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-08-29 21:28:07 +0530

    add: Support for Dynamic Delimiters

--- src/main/java/com/dhwanil/task/Calculator.java
+++ src/main/java/com/dhwanil/task/Calculator.java
@@ -2,6 +2,15 @@ package com.dhwanil.task;
 
 public class Calculator {
 
+    public String[] split(String numbers) {
+        String sep = ",";
+        if (numbers.startsWith("//")) {
+            sep = String.valueOf(numbers.charAt(2));
+            numbers = numbers.substring(4);
+        }
+        return split(numbers, sep);
+    }
+
     public String[] split(String numbers, String sep) {
         return numbers.replace("\n", sep).split(sep);
     }
@@ -9,7 +18,7 @@ public class Calculator {
     public int Add(String numbers) {
         if (numbers.length() == 0) return 0;
         else {
-            String[] tokens = split(numbers, ",");
+            String[] tokens = split(numbers);
             int sum = 0;
             for (String str : tokens) {
                 sum += Integer.parseInt(str);
