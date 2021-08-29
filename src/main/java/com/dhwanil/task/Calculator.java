0cb68daffb1ab9619847cc58cf40dc230e05f17b
dhwanil-ditani <ditanidhwanil@gmail.com>
2021-08-29 20:54:00 +0530

    fix for test case multiple number input

--- src/main/java/com/dhwanil/task/Calculator.java
+++ src/main/java/com/dhwanil/task/Calculator.java
@@ -1,8 +1,17 @@
 package com.dhwanil.task;
 
+import java.util.Arrays;
+
 public class Calculator {
     public int Add(String numbers) {
         if (numbers.length() == 0) return 0;
-        else return Integer.parseInt(numbers);
+        else {
+            String[] tokens = numbers.split(",");
+            int sum = 0;
+            for (String str : tokens) {
+                sum += Integer.parseInt(str);
+            }
+            return sum;
+        }
     }
 }
